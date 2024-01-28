5bf2eb3a40a78352e5a849170f63dea7f405eac8
Teodor <teodorttodorov@icloud.com>
2024-01-28 20:14:04 +0200

    URLSession & Networking

--- WeatherManager.swift
+++ WeatherManager.swift
@@ -19,9 +19,23 @@ struct WeatherManager {
     
     func performRequest(urlString: String) {
         //1. Create URL
-        //2. Create URLSession
-        //3. Give the session a task
-        //4. Start a task
+        
+        if let url = URL(string: urlString) {
+            
+            //2. Create URLSession
+            
+            let session = URLSession(configuration: .default)
+            //3. Give the session a task
+            
+            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            //4. Start a task
+            task.resume()
+        }
+    }
+    
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        
     }
     
 }
