8f2753a384b57c2377862fc18c29c47bc151fba7
Teodor <teodorttodorov@icloud.com>
2024-02-01 19:53:23 +0200

    Return Value

---
+++ Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Angela Yu on 01/09/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    
+    
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    
+    var weatherManager = WeatherManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchTextField.delegate = self
+    }
+    
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if searchTextField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+        }
+        
+    }
+        func textFieldDidEndEditing(_ textField: UITextField) {
+            
+            if let city = searchTextField.text {
+                weatherManager.fetchWeather(cityName: city)
+            }
+            
+            searchTextField.text = ""
+        }
+         
+    
+    
+}
+// API Key -  74a47217a94036523b25789d068ac02c
---
+++ WeatherData.swift
@@ -0,0 +1,27 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Teo on 1.02.24.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct WeatherData: Decodable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+
+struct Main: Decodable {
+    let temp: Double
+}
+
+struct Weather: Decodable {
+    let id: Int
+}
+
+
