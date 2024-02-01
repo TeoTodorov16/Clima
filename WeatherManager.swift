b2eac4adb6b13e5b7a775d4407771ed0be4bbb92
Teodor <teodorttodorov@icloud.com>
2024-02-01 19:05:41 +0200

    Changed it a little bit

--- WeatherManager.swift
+++ WeatherManager.swift
@@ -53,19 +53,29 @@ struct WeatherManager {
     }
     
     func getConditionName(weatherID: Int) -> String {
-        let result = weatherID
         
-        switch result {
-        case 803:
+        switch weatherID {
+        case 200...232:
+            return "cloud.bolt"
+        case 300...321:
+            return "cloud.drizzle"
+        case 500...531:
             return "cloud.rain"
-        case 802:
-            return "cloud.rain.fill"
-        case 801:
-            return "cloud.rain.circle"
+        case 600...622:
+            return "cloud.snow"
+        case 701...781:
+            return "cloud.fog"
+        case 800:
+            return "sun.max"
+        case 801...804:
+            return "cloud.bolt"
         default:
-            return "idk"
+            return "cloud"
         }
+        
+        
     }
+    
     
 }
 
