2ecb4fcf18adb7c61d6f1a1257e692366aa3f2de
Teodor <teodorttodorov@icloud.com>
2024-02-01 13:06:10 +0200

    Making the Handler & Functions into a Closure

--- WeatherManager.swift
+++ WeatherManager.swift
@@ -23,27 +23,27 @@ struct WeatherManager {
         if let url = URL(string: urlString) {
             
             //2. Create URLSession
-            
             let session = URLSession(configuration: .default)
-            //3. Give the session a task
             
-            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            //3. Give the session a task
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    let dataString = String(data: safeData, encoding: .utf8)
+                    print(dataString)
+                }
+            }
+            
             //4. Start a task
             task.resume()
         }
     }
     
     
-    func handle(data: Data?, response: URLResponse?, error: Error?) {
-        if error != nil {
-            print(error!)
-            return
-        }
-        
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString)
-        }
-    }
+    
     
 }
