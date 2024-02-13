c22d74c91e3af326ec8a7fd24ce50efc4f46173c
Teodor <teodorttodorov@icloud.com>
2024-02-13 18:07:55 +0200

    Updating th UI to show the ouput of the Data from API.

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -59,7 +59,10 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
         }
          
     func didUpdateWeather(_ weatherManager: WeatherManager,  weather: WeatherModel) {
-        print(weather.temperature)
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString
+            self.conditionImageView.image = UIImage(systemName: weather.ConditionName)
+        }
     }
     
     func didFailWithError(error: Error) {
